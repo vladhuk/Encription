4babb909b453e0be44b10d9dc8969a7a921d2e5d
vladhuk <vladhookovskiy@gmail.com>
2019-09-08 23:04:16 +0300

    Add tests;

---
+++ src/test/java/com/vladhuk/encription/encryptor/CaesarEncryptorTest.java
@@ -0,0 +1,47 @@
+package com.vladhuk.encription.encryptor;
+
+import com.vladhuk.encription.Encryptor;
+import com.vladhuk.util.Language;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CaesarEncryptorTest {
+
+    @Test
+    public void encode_symbol_When_LanguageCorrect_Expected_CorrectResult() {
+        final Encryptor encryptor = new CaesarEncryptor(Language.ENGLISH, 1);
+        final char symbol = 'H';
+        final char expectedSymbol = 'I';
+
+        assertEquals(expectedSymbol, encryptor.encode(symbol));
+    }
+
+    @Test
+    public void encode_string_When_LanguageCorrect_Expected_CorrectResult() {
+        final Encryptor encryptor = new CaesarEncryptor(Language.ENGLISH, 1);
+        final String text = "Hello! Z";
+        final String expectedText = "Ifmmp! A";
+
+        assertEquals(expectedText, encryptor.encode(text));
+    }
+
+    @Test
+    public void decode_symbol_When_LanguageCorrect_Expected_CorrectResult() {
+        final Encryptor encryptor = new CaesarEncryptor(Language.ENGLISH, 1);
+        final char symbol = 'I';
+        final char expectedSymbol = 'H';
+
+        assertEquals(expectedSymbol, encryptor.decode(symbol));
+    }
+
+    @Test
+    public void decode_string_When_LanguageCorrect_Expected_CorrectResult() {
+        final Encryptor encryptor = new CaesarEncryptor(Language.ENGLISH, 1);
+        final String text = "Ifmmp! A";
+        final String expectedText = "Hello! Z";
+
+        assertEquals(expectedText, encryptor.decode(text));
+    }
+
+}
